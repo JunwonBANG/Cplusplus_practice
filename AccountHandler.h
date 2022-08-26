91f76400f5e5dcd2492c13bcedd2b74fca79d5d6
JunwonBANG <bjwgoodboy@gmail.com>
2022-08-27 03:06:51 +0900

    new file

---
+++ AccountHandler.h
@@ -0,0 +1,25 @@
+#ifndef __ACCOUNT_HANDLER_H__
+#define __ACCOUNT_HANDLER_H__
+
+#inclue "Account.h"
+
+class AccountHandler
+{
+	private:
+		Account * accArr[100];
+		int accNum;
+		
+	public:
+		AccountHandler();
+		void ShowMenu(void) const;
+		void MakeAccount(void);
+		void DepositMoney(void);
+		void WithdrawMoney(void);
+		void ShowAllAccInfo(void);
+		~AccountHandler();
+		
+	protected:
+		void MakeNormalAccount(void);
+		void MakeCreditAccount(void);
+};
+#endif
